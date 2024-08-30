83b30318798ff32a88dc75d95b918728b78347b8
Affaan <maffaanahmed@gmail.com>
2024-08-31 04:26:25 +0500

    Admin Login Page Setup

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,36 @@
+const adminModel = require("../models/adminModel");
+const { responseReturn } = require("../utils/response");
+const bcrypt = require("bcrypt");
+const { createToken } = require("../utils/tokenCreate");
+
 class authControllers {
   admin_login = async (req, res) => {
-    console.log(req.body);
+    const { email, password } = req.body;
+    try {
+      const admin = await adminModel.findOne({ email }).select("+password");
+      // console.log(admin);
+      if (admin) {
+        const match = await bcrypt.compare(password, admin.password);
+        // console.log(match);
+        if (match) {
+          const token = createToken({
+            id: admin.id,
+            role: admin.role,
+          });
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, { token, message: "Login Success" });
+        } else {
+          responseReturn(res, 404, { error: "Password is incorrect" });
+        }
+      } else {
+        responseReturn(res, 401, { message: "Invalid email" });
+        return;
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
+    }
   };
 }
 
